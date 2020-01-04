916fda8e433c5a0e23adf83f40b05a3b48d0d17f
Omar Cedeño <grarogccee@gmail.com>
2020-01-04 16:30:31 -0500

    metodoDEletePersonas

--- webServer/src/controllers/index.controller.js
+++ webServer/src/controllers/index.controller.js
@@ -12,13 +12,34 @@ const getPersonas= async(req,res)=>{
    const response= await pool.query('SELECT * FROM personas');
    res.json(response.rows);
 };
+
+
+
+
 const createPersonas=async(req,res)=>{
 const {nombre,cedula,clave,direccion,email,telefono,tipopersona}=req.body;
 
-const response = await pool.query('INSERT INTO personas(nombre,cedula,clave,direccion,email,telefono,tipopersona) VALUES($1,$2,$3,$4,$5,$6,$7)',[nombre,cedula,clave,direccion,email,telefono,tipopersona])
-res.send('usuario creado')
+const response = await pool.query('INSERT INTO personas(nombre,cedula,clave,direccion,email,telefono,tipopersona) VALUES($1,$2,$3,$4,$5,$6,$7)',[nombre,cedula,clave,direccion,email,telefono,tipopersona]);
+res.json({
+    massage:'usuarios creado',
+    body:{
+        user:{nombre,cedula,clave,direccion,email,telefono,tipopersona}
+    }
+   })
+};
+
+
+
+
+const deletePersonas = async (req, res) => {
+    const cedula = parseInt(req.params.cedula);
+    await pool.query('DELETE FROM personas where cedula = $1', [
+        cedula
+    ]);
+    res.json(`persona ${cedula} eliminada`);
 };
 module.exports={
     getPersonas,
-    createPersonas
+    createPersonas,
+    deletePersonas
 }
--- webServer/src/routes/index.js
+++ webServer/src/routes/index.js
@@ -1,9 +1,10 @@
 const {Router} =require('express');
 const router =Router();
-const {getPersonas,createPersonas}= require('../controllers/index.controller')
+const {getPersonas,createPersonas,deletePersonas}= require('../controllers/index.controller')
 
 
 
 router.get('/personas',getPersonas);
 router.post('/crearPersonas',createPersonas);
+router.delete('/deletePersonas/:cedula',deletePersonas);
 module.exports =router;
