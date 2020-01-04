a6057e580b4b096b7ff73f3133eda59a887450d5
Omar Cedeño <grarogccee@gmail.com>
2020-01-04 17:05:57 -0500

    deleteCartillasPersona

--- webServer/src/controllers/index.controller.js
+++ webServer/src/controllers/index.controller.js
@@ -7,18 +7,14 @@ password:'roger130296',
 database:'Bingo',
 post:'5432'
 });
-//tabla Personas
+//tabla Personas Get 
 const getPersonas= async(req,res)=>{
    const response= await pool.query('SELECT * FROM personas');
    res.json(response.rows);
 };
-
-
-
-
+//tabla Personas Create
 const createPersonas=async(req,res)=>{
 const {nombre,cedula,clave,direccion,email,telefono,tipopersona}=req.body;
-
 const response = await pool.query('INSERT INTO personas(nombre,cedula,clave,direccion,email,telefono,tipopersona) VALUES($1,$2,$3,$4,$5,$6,$7)',[nombre,cedula,clave,direccion,email,telefono,tipopersona]);
 res.json({
     massage:'usuarios creado',
@@ -27,10 +23,7 @@ res.json({
     }
    })
 };
-
-
-
-
+//tabla Personas Delete
 const deletePersonas = async (req, res) => {
     const cedula = parseInt(req.params.cedula);
     await pool.query('DELETE FROM personas where cedula = $1', [
@@ -38,8 +31,43 @@ const deletePersonas = async (req, res) => {
     ]);
     res.json(`persona ${cedula} eliminada`);
 };
+
+
+
+
+// tabla PersonaCartilla get 
+const PerCartilla= async(req,res)=>{
+    const response= await pool.query('SELECT * FROM personacartilla');
+    res.json(response.rows);
+ };
+ //tabla PersonaCartilla Create
+const createPerCartilla=async(req,res)=>{
+    const {fkcartilla,fkpersona}=req.body;
+    const response = await pool.query('INSERT INTO personacartilla(fkcartilla,fkpersona) VALUES($1,$2)',[fkcartilla,fkpersona]);
+    res.json({
+        massage:'personacartilla creada',
+        body:{
+            user:{fkcartilla,fkpersona}
+        }
+       })
+    };
+    //tabla Personas Delete
+const deletePerCartilla = async (req, res) => {
+    const id = parseInt(req.params.id);
+    await pool.query('DELETE FROM personacartilla where id = $1', [
+        id
+    ]);
+    res.json(`personacartilla ${id} eliminada`);
+};
+
 module.exports={
+    //metodos de la tabla Personas
     getPersonas,
     createPersonas,
-    deletePersonas
+    deletePersonas,
+
+    //metodos de la tablaPersonaCartilla
+    PerCartilla,
+    createPerCartilla,
+    deletePerCartilla
 }
--- webServer/src/routes/index.js
+++ webServer/src/routes/index.js
@@ -1,10 +1,21 @@
 const {Router} =require('express');
 const router =Router();
-const {getPersonas,createPersonas,deletePersonas}= require('../controllers/index.controller')
+const {
+    getPersonas,
+    createPersonas,
+    deletePersonas,
+    PerCartilla,
+    createPerCartilla,
+    deletePerCartilla}= require('../controllers/index.controller')
 
 
-
+//rutas Tabla Personas
 router.get('/personas',getPersonas);
 router.post('/crearPersonas',createPersonas);
 router.delete('/deletePersonas/:cedula',deletePersonas);
+
+//rutas Tabla PerCartilla
+router.get('/perCartilla',PerCartilla);
+router.post('/crearPerCartilla',createPerCartilla);
+router.delete('/deletePerCartilla/:id',deletePerCartilla);
 module.exports =router;
